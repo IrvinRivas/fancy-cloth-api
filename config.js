4e978770fc304d1176e87573e7da88095b7f0304
irvin <irvinvegarivas@gmail.com>
2021-04-08 21:01:23 -0600

    añadiendo configuracion

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    dev:{
+        host:"https://fancy-cloth-api.herokuapp.com/:",
+        port:3005
+    },
+    mysql:{
+        host:"busyodektookpf3rxqyq-mysql.services.clever-cloud.com",
+        user:"uwrcsgajodkjsl9z",
+        password:"Jin1uVNOZI2DGURfBeUd",
+        database:"busyodektookpf3rxqyq"
+    },
+    secret:'secretillo234'  
+}
+
+module.exports= config;
